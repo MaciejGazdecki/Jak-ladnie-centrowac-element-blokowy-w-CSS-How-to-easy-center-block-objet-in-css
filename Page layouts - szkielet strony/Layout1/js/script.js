e826c503abeaefc6a4615593ac338be1431a66af
Maciej Gazdecki <maciej.gazdecki@gmail.com>
2019-06-09 17:53:33 +0200

    jQuery to JS conversion

--- Page layouts - szkielet strony/Layout1/js/script.js
+++ Page layouts - szkielet strony/Layout1/js/script.js
@@ -1,27 +1,42 @@
-function showmenu() {
-    const burger = document.querySelector(".burger");
-    const nav = document.querySelector(".menu")
-    burger.addEventListener('click', function () {
-        nav.classList.toggle('on');
-    });
-}
-showmenu();
-function showArrow () {
-    $(window).on("scroll", function () {
-        if ($(window).scrollTop() > 180) {
-            $('.arrow').removeClass('on')
-        }
-        else {
-            $('.arrow').addClass('on')
-        }
-    });
-}
-showArrow();
-function arrow () {
-    $('.arrow').on('click', function () {
-        $("html").animate({
-            scrollTop: 0,
+function pageEfects() {
+    function showmenu() {
+        const burger = document.querySelector(".burger");
+        const nav = document.querySelector(".menu")
+        burger.addEventListener('click', function () {
+            nav.classList.toggle('on');
+        });
+    }
+    showmenu();
+
+    function showArrow() {
+        //wersja jQuery
+        // $(window).on("scroll", function () {
+        //     if ($(window).scrollTop() > 180) {
+        //         $('.arrow').removeClass('on')
+        //     }
+        //     else {
+        //         $('.arrow').addClass('on')
+        //     }
+        // });
+        //wersja js vanilla
+        const arrow = document.querySelector(".arrow")
+        document.addEventListener("scroll", function () {
+            if (window.scrollY > 180) {
+                arrow.classList.remove('on')
+            } else {
+                arrow.classList.add('on')
+            }
         })
-    });
+    }
+    showArrow();
+
+    function arrowScroll() {
+        $('.arrow').on('click', function () {
+            $("html").animate({
+                scrollTop: 0,
+            })
+        });
+    }
+    arrowScroll();
 }
-arrow();
+pageEfects();
