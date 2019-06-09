612220c3b1ddd9311a193cca770268a3e81e8a32
Maciej Gazdecki <maciej.gazdecki@gmail.com>
2019-06-09 15:02:11 +0200

    animacja strzalki

--- Page layouts - szkielet strony/Layout1/js/script.js
+++ Page layouts - szkielet strony/Layout1/js/script.js
@@ -1,11 +1,22 @@
-function Showmenu() {
+function showmenu() {
     const burger = document.querySelector(".burger");
     const nav = document.querySelector(".menu")
     burger.addEventListener('click', function () {
         nav.classList.toggle('on');
     });
 }
-Showmenu();
+showmenu();
+function showArrow () {
+    $(window).on("scroll", function () {
+        if ($(window).scrollTop() > 180) {
+            $('.arrow').removeClass('on')
+        }
+        if ($(window).scrollTop() < 180) {
+            $('.arrow').addClass('on')
+        }
+    });
+}
+showArrow();
 function arrow () {
     $('.arrow').on('click', function () {
         $("html").animate({
